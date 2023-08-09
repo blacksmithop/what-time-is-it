c4af66d99112c50091653e660df3a66e5c32468b
Abhinav <60320192+blacksmithop@users.noreply.github.com>
2023-08-09 04:13:24 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from what_time_is_it import __version__
+
+setup(
+    name='what-time-is-it',
+    version=__version__,
+
+    url='https://github.com/blacksmithop/what-time-is-it',
+    author='Abhinav KM',
+    author_email='angstycoder101@gmail.com',
+
+    py_modules=['what-time-is-it'],
+)
---
+++ what_time_is_it.py
@@ -0,0 +1,4 @@
+def hello_world():
+    print("Current time is 10:10")
+
+__version__ = 'v1.0.0'
